29876caea68321242ba8e87d2aca8e190aec893b
Thirafi Wian <thirafi.badai@gmail.com>
2018-10-10 20:40:48 +0700

    fux

--- public/javascripts/countdown.js
+++ public/javascripts/countdown.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Oct 10, 2018 18:00:00").getTime();
+var countDownDate = new Date("Oct 13, 2018 23:59:59").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
@@ -23,6 +23,6 @@ var x = setInterval(function() {
   // If the count down is finished, write some text 
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("cntd").innerHTML = "Masa plotting sudah berakhir";
+    document.getElementById("cntd").innerHTML = "Masa submisi sudah berakhir";
   }
 }, 1000);
