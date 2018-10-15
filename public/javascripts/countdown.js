bbaa26889e978fcd946a3c22c744dd673f81f9f8
Thirafi Wian <thirafi.badai@gmail.com>
2018-10-15 11:51:42 +0700

    phase 1

--- public/javascripts/countdown.js
+++ public/javascripts/countdown.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Oct 13, 2018 23:59:59").getTime();
+var countDownDate = new Date("Oct 21, 2018 08:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
@@ -23,6 +23,6 @@ var x = setInterval(function() {
   // If the count down is finished, write some text 
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("cntd").innerHTML = "Masa submisi sudah berakhir";
+    document.getElementById("cntd").innerHTML = "Phase 1 telah berakhir";
   }
 }, 1000);
